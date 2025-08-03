54f0b4a42e222b6eef823a365ff16cbdd7b18cfb
vncntcmnn <vincentcamenen@gmail.com>
2025-08-03 17:57:20 +0200

    Add DyHead

--- src/feneck/__init__.py
+++ src/feneck/__init__.py
@@ -1,5 +1,6 @@
 from feneck.custom_pan import CustomCSPPAN
+from feneck.dyhead import DyHead
 from feneck.fpn import FPN
 from feneck.simple_fpn import SimpleFPN
 
-__all__ = ["CustomCSPPAN", "FPN", "SimpleFPN"]
+__all__ = ["CustomCSPPAN", "DyHead", "FPN", "SimpleFPN"]
