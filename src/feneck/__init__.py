5a11ec0db207bad1cde368890b57d9dadea6fa11
vncntcmnn <vincentcamenen@gmail.com>
2025-08-09 17:13:53 +0200

    Add HRFPN

--- src/feneck/__init__.py
+++ src/feneck/__init__.py
@@ -2,6 +2,7 @@ from feneck.bifpn import BiFPN
 from feneck.custom_pan import CustomCSPPAN
 from feneck.dyhead import DyHead
 from feneck.fpn import FPN
+from feneck.hrfpn import HRFPN
 from feneck.simple_fpn import SimpleFPN
 
-__all__ = ["BiFPN", "CustomCSPPAN", "DyHead", "FPN", "SimpleFPN"]
+__all__ = ["BiFPN", "CustomCSPPAN", "DyHead", "FPN", "HRFPN", "SimpleFPN"]
