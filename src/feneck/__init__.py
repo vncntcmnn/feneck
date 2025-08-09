10c1c8d2e5c8fbb4dcc4dc24d6c28d270b072230
vncntcmnn <vincentcamenen@gmail.com>
2025-08-09 17:05:03 +0200

    Add BiFPN

--- src/feneck/__init__.py
+++ src/feneck/__init__.py
@@ -1,6 +1,7 @@
+from feneck.bifpn import BiFPN
 from feneck.custom_pan import CustomCSPPAN
 from feneck.dyhead import DyHead
 from feneck.fpn import FPN
 from feneck.simple_fpn import SimpleFPN
 
-__all__ = ["CustomCSPPAN", "DyHead", "FPN", "SimpleFPN"]
+__all__ = ["BiFPN", "CustomCSPPAN", "DyHead", "FPN", "SimpleFPN"]
